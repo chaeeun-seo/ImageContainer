270ee4b3ce707ca516be8d7c6e506b307dd391ad
chaeeun-seo <seochaeeun961126@gmail.com>
2023-09-17 20:08:21 +0900

    add tailwindcss

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <h1 className='font-bold underline bg-blue-500'>HELLO</h1>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
