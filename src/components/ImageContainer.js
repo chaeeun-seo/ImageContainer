207c378f69588405554115e8c274a7a12acb0f65
chaeeun-seo <seochaeeun961126@gmail.com>
2023-09-18 13:59:51 +0900

    change ImageContainer to grid

--- src/components/ImageContainer.js
+++ src/components/ImageContainer.js
@@ -7,8 +7,8 @@ export default function ImageContainer() {
     const items = dummyItems.map((item) => (<ItemContainer key={item.id} name={item.name} image_url={item.image_url} price={item.price}/>))
 
     return (
-        // <div className='grid lg:grid-cols-5 md:grid-cols-3 grid-cols-2 gap-4'>     
-        <div className='flex flex-wrap gap-4 justify-start justify-items-center'>
+        // <div className='w-11/12 sm:w-9/12 flex flex-wrap gap-4 justify-start justify-items-center'>
+        <div className='grid lg:grid-cols-5 md:grid-cols-3 grid-cols-2 gap-4'>     
             {items}
         </div>
     )
