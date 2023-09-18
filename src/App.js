41f41d02d473edcc9e08467b5b82453fb1c99666
chaeeun-seo <seochaeeun961126@gmail.com>
2023-09-18 15:52:07 +0900

    add HorizontalContainer in App component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import GridContainer from './components/GridContainer';
 import CategoryContainer from './components/CategoryContainer';
+import HorizontalContainer from './components/HorizontalContainer';
 
 function App() {
   return (
     <div className='w-full flex flex-col justify-center items-center gap-4'>
       <CategoryContainer/>
+      <HorizontalContainer/>
       <GridContainer/>
     </div>
   );
