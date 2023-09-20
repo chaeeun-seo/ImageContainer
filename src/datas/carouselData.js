e9fb4a566cd15448e24931e6726f834eebfa19c2
chaeeun-seo <seochaeeun961126@gmail.com>
2023-09-20 09:47:21 +0900

    add dummy carouselData and banner images for CarouselBox and CarouselContainer

---
+++ src/datas/carouselData.js
@@ -0,0 +1,30 @@
+export default {
+    "carousel" : {
+        "items" : [
+            // 데이터 4개 이상이어야 함
+            {
+                "carousel_id" : 1,
+                "carousel_image_url" : "/banners/banner_1.png",
+                // "https://play-lh.googleusercontent.com/43cAxUW2TUUGRbXexRlYdlr8fGHLN2ay3Z9IieN5THJRvA5UeWXAbNZ4_3hHoaujY1U=w1024",
+            },
+            {
+                "carousel_id" : 2,
+                "carousel_image_url" : "/banners/banner_2.png",
+                // "https://ifland.io/movie/thumnail/ifland_desktop1.png",
+            },
+            {
+                "carousel_id" : 3,
+                "carousel_image_url" : "/banners/banner_3.png",
+                // "https://www.koreaittimes.com/news/photo/202305/121836_69742_243.jpg",
+            },
+            {
+                "carousel_id" : 4,
+                "carousel_image_url" : "/banners/banner_4.png",
+            },
+            {
+                "carousel_id" : 5,
+                "carousel_image_url" : "/banners/banner_5.png",
+            },
+        ],
+    }
+}
