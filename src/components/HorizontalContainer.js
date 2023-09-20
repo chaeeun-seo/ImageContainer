a76fefbc4229328b0485b9410530fdb3839a485f
chaeeun-seo <seochaeeun961126@gmail.com>
2023-09-20 11:04:07 +0900

    add title to HorizontalContainer

--- src/components/HorizontalContainer.js
+++ src/components/HorizontalContainer.js
@@ -5,10 +5,14 @@ import ItemContainer from './ItemContainer'
 export default function HorizontalContainer() {
     const dummyItems = dummyData.data.items
     const items = dummyItems.map((item) => (<ItemContainer key={item.id} checkGrid={false} name={item.name} image_url={item.image_url} price={item.price}/>))
+    const title = dummyData.title
 
     return (
-        <div className='w-full flex flex-row gap-4 overflow-x-auto'>
-            {items}
-        </div>
+        <>
+            <h2 className='w-full text-left text-lg font-bold'>{title}</h2>
+            <div className='w-full flex flex-row gap-4 overflow-x-auto'>
+                {items}
+            </div>
+        </>
     )
 }
