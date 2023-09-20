260d2138378c152c869579c466b673357b7f873c
chaeeun-seo <seochaeeun961126@gmail.com>
2023-09-20 09:49:14 +0900

    change type of category_id from string to int

--- src/datas/categoryData.js
+++ src/datas/categoryData.js
@@ -2,47 +2,47 @@ export default {
     "category" : {
         "items" :[
             {
-                "category_id" : "1",
+                "category_id" : 1,
                 "category_name" : "한벌 옷",
                 "category_icon" : "/icons/한벌옷.png",
             },
             {
-                "category_id" : "2",
+                "category_id" : 2,
                 "category_name" : "상의",
                 "category_icon" : "/icons/상의.png",
             },
             {
-                "category_id" : "3",
+                "category_id" : 3,
                 "category_name" : "바지",
                 "category_icon" : "/icons/하의.png",
             },
             {
-                "category_id" : "4",
+                "category_id" : 4,
                 "category_name" : "아우터",
                 "category_icon" : "/icons/아우터.png",
             },
             {
-                "category_id" : "5",
+                "category_id" : 5,
                 "category_name" : "원피스",
                 "category_icon" : "/icons/원피스.png",
             },
             {
-                "category_id" : "6",
+                "category_id" : 6,
                 "category_name" : "신발",
                 "category_icon" : "/icons/신발.png",
             },
             {
-                "category_id" : "7",
+                "category_id" : 7,
                 "category_name" : "가방",
                 "category_icon" : "/icons/가방.png",
             },
             {
-                "category_id" : "8",
+                "category_id" : 8,
                 "category_name" : "가방",
                 "category_icon" : "/icons/가방.png",
             },
             {
-                "category_id" : "9",
+                "category_id" : 9,
                 "category_name" : "가방",
                 "category_icon" : "/icons/가방.png",
             },
