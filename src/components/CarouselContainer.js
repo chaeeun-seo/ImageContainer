bdb3f7b9a4a27bed7db48bc6495dc71febff59f5
chaeeun-seo <seochaeeun961126@gmail.com>
2023-09-20 10:35:44 +0900

    make CarouselContainer that holds several CarouselBox

--- src/components/CarouselContainer.js
+++ src/components/CarouselContainer.js
@@ -1,7 +1,19 @@
 import React from 'react'
+import carouselData from '../datas/carouselData'
+import CarouselBox from './CarouselBox'
 
 export default function CarouselContainer() {
+    const carouselItems = carouselData.carousel.items
+    const carouselBoxes = carouselItems.map((item) => (<CarouselBox key={item.carousel_id} image={item.carousel_image_url}/>))
+    const count = carouselItems.length
+    
     return (
-        <div>CarouselContainer</div>
+        <div id="default-carousel" className="relative w-full" data-carousel="slide">
+            {/* Carousel Wrapper */}
+            <div className="relative h-56 overflow-hidden md:h-72">
+                {carouselBoxes}
+            </div>
+            
+        </div>
     )
 }
