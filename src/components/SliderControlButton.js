32df6e5e46d281e8ac7a84fd64cbe0f654197b70
chaeeun-seo <seochaeeun961126@gmail.com>
2023-09-20 16:25:04 +0900

    create SliderControlButton component

---
+++ src/components/SliderControlButton.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function SliderControlButton({path}) {
+    return (
+        <span className="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
+            <svg className="w-4 h-4 text-white dark:text-gray-800" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
+                <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d={path}/>
+            </svg>
+            <span className="sr-only">Previous</span>
+        </span>
+    )
+}
