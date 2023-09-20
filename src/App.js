b9fb13c1a00858a85b9f9d9690726b45a6925ac2
chaeeun-seo <seochaeeun961126@gmail.com>
2023-09-20 09:53:39 +0900

    import Carousel from flowbite and add CarouselContainer to App component

--- src/App.js
+++ src/App.js
@@ -2,10 +2,13 @@ import './App.css';
 import GridContainer from './components/GridContainer';
 import CategoryContainer from './components/CategoryContainer';
 import HorizontalContainer from './components/HorizontalContainer';
+import { Carousel } from 'flowbite';
+import CarouselContainer from './components/CarouselContainer';
 
 function App() {
   return (
     <div className='w-full flex flex-col justify-center items-center gap-4'>
+      <CarouselContainer/>
       <CategoryContainer/>
       <HorizontalContainer/>
       <GridContainer/>
