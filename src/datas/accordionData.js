c3e1e20932c584f9538675f335a6bce48bdd059a
chaeeun-seo <seochaeeun961126@gmail.com>
2023-09-21 20:26:51 +0900

    make dummy accordionData for AccordionContainer and AccordionBox

---
+++ src/datas/accordionData.js
@@ -0,0 +1,178 @@
+export default {
+    "success" : true,
+    "title" : "이번주 크리에이터 순위",
+    "accordion" : {
+        "creators" : [
+            {
+                "rank" : 1,
+                "id" : 100, // 크리에이터 식별
+                "name" : "도니",
+                "tags" : [
+                    "HOT",
+                    "블랙앤화이트",
+                    "스트릿",
+                ],
+                "new_badge" : false,
+                "profile_image" : "https://celclipmaterialprod.s3-ap-northeast-1.amazonaws.com/39/88/8839/example",
+                "items" : [
+                    {
+                        "id" : 1,
+                        "image_url" : "/assets/image_16.png",
+                    },
+                    {
+                        "id" : 2,
+                        "image_url" : "/assets/image_17.png",
+                    },
+                    {
+                        "id" : 3,
+                        "image_url" : "/assets/image_18.png",
+                    },
+                    {
+                        "id" : 4,
+                        "image_url" : "/assets/image_18.png",
+                    },
+                    {
+                        "id" : 5,
+                        "image_url" : "/assets/image_18.png",
+                    },
+                ],
+            },
+            {
+                "rank" : 2,
+                "id" : 200,
+                "name" : "뀨르",
+                "tags" : [
+                    "트렌드",
+                    "힙앤시크",
+                    "유니크",
+                ],
+                "new_badge" : false,
+                "profile_image" : "/assets/image_18.png",
+                "items" : [
+                    {
+                        "id" : 1,
+                        "image_url" : "/assets/image_16.png",
+                    },
+                    {
+                        "id" : 2,
+                        "image_url" : "/assets/image_17.png",
+                    },
+                    {
+                        "id" : 3,
+                        "image_url" : "/assets/image_18.png",
+                    },
+                    {
+                        "id" : 4,
+                        "image_url" : "/assets/image_18.png",
+                    },
+                    {
+                        "id" : 5,
+                        "image_url" : "/assets/image_18.png",
+                    },
+                ],
+            },
+            {
+                "rank" : 3,
+                "id" : 300,
+                "name" : "ㅇㅇㅁ",
+                "tags" : [
+                    "NEW",
+                    "영앤리치",
+                    "젠더리스",
+                ],
+                "new_badge" : false,
+                "profile_image" : "/assets/image_18.png",
+                "items" : [
+                    {
+                        "id" : 1,
+                        "image_url" : "/assets/image_16.png",
+                    },
+                    {
+                        "id" : 2,
+                        "image_url" : "/assets/image_17.png",
+                    },
+                    {
+                        "id" : 3,
+                        "image_url" : "/assets/image_18.png",
+                    },
+                    {
+                        "id" : 4,
+                        "image_url" : "/assets/image_18.png",
+                    },
+                    {
+                        "id" : 5,
+                        "image_url" : "/assets/image_18.png",
+                    },
+                ],
+            },
+            {
+                "rank" : 4,
+                "id" : 400,
+                "name" : "호랭이",
+                "tags" : [
+                    "야옹",
+                    "어흥",
+                    "디스이즈힙"
+                ],
+                "new_badge" : true,
+                "profile_image" : "https://img.freepik.com/free-vector/tiger-pattern-hand-drawn-design_23-2148212815.jpg",
+                "items" : [
+                    {
+                        "id" : 1,
+                        "image_url" : "/assets/image_16.png",
+                    },
+                    {
+                        "id" : 2,
+                        "image_url" : "/assets/image_17.png",
+                    },
+                    {
+                        "id" : 3,
+                        "image_url" : "/assets/image_18.png",
+                    },
+                    {
+                        "id" : 4,
+                        "image_url" : "/assets/image_18.png",
+                    },
+                    {
+                        "id" : 5,
+                        "image_url" : "/assets/image_18.png",
+                    },
+                ],
+            },
+            {
+                "rank" : 5,
+                "id" : 500,
+                "name" : "CHANNEL",
+                "tags" : [
+                    "THE",
+                    "TRUE",
+                    "ONE"
+                ],
+                "new_badge" : true,
+                "profile_image" : "https://blog.kakaocdn.net/dn/bYX0hu/btrCqGQWsaK/LHtUbDsW4lbLqUTflAdU7K/img.png",
+                "items" : [
+                    {
+                        "id" : 1,
+                        "image_url" : "/assets/image_16.png",
+                    },
+                    {
+                        "id" : 2,
+                        "image_url" : "/assets/image_17.png",
+                    },
+                    {
+                        "id" : 3,
+                        "image_url" : "/assets/image_18.png",
+                    },
+                    {
+                        "id" : 4,
+                        "image_url" : "/assets/image_18.png",
+                    },
+                    {
+                        "id" : 5,
+                        "image_url" : "/assets/image_18.png",
+                    },
+                ],
+            },
+        ],
+    },
+}
