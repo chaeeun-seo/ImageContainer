68026c209ba4e5bbaf334be208098d27206ebd2a
chaeeun-seo <seochaeeun961126@gmail.com>
2023-09-22 11:51:33 +0900

    add title to HorizontalContainer using isTitle prop and put title and ItemContainers in the flex with gap depending on isTitle prop

--- src/components/HorizontalContainer.js
+++ src/components/HorizontalContainer.js
@@ -2,17 +2,18 @@ import React from 'react'
 import dummyData from '../datas/dummyData'
 import ItemContainer from './ItemContainer'
 
-export default function HorizontalContainer() {
+// HorizontalContainer receives props that decides which data to get
+export default function HorizontalContainer({isTitle}) {
     const dummyItems = dummyData.data.items
     const items = dummyItems.map((item, index) => (<ItemContainer key={item.id} order={index} checkGrid={false} name={item.name} image_url={item.image_url} price={item.price}/>))
-    const title = dummyData.title
+    const title = isTitle ? dummyData.title : ""
 
     return (
-        <>
-            <h2 className='w-full text-left text-lg font-bold'>{title}</h2>
-            <div className='w-full flex flex-row gap-4 overflow-x-auto'>
+        <div className={`w-full flex flex-col justify-center items-center ${isTitle?'gap-3':'gap-0'}`}>
+            <h2 className='w-full max-w-6xl text-left text-lg font-bold'>{title}</h2>
+            <div className='w-full max-w-6xl flex flex-row gap-4 overflow-x-auto'>
                 {items}
             </div>
-        </>
+        </div>
     )
 }
