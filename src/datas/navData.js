11561940a98941758e18f5aa097974179bc7b03d
chaeeun-seo <seochaeeun961126@gmail.com>
2023-09-22 22:01:59 +0900

    create navData that holds info about nav link

---
+++ src/datas/navData.js
@@ -0,0 +1,32 @@
+export const navLinks = [
+    {
+        id : "공지사항",
+        title : "공지사항",
+        icon : false,
+    },
+    {
+        id : "도움말", 
+        title : "도움말",
+        icon : false,
+    },
+    {
+        id : "통계",
+        title : "통계",
+        icon : true,
+    },
+    {
+        id : "내 프로젝트",
+        title : "내 프로젝트",
+        icon : true,
+    },
+    {
+        id : "로그인",
+        title : "로그인",
+        icon : true,
+    },
+    {
+        id : "한국어",
+        title : "한국어",
+        icon : false,
+    }
+]
