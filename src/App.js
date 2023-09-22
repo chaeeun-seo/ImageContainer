d97ec47642cb63bdbbcda846f7836b8640478170
chaeeun-seo <seochaeeun961126@gmail.com>
2023-09-22 22:02:47 +0900

    put Navbar in App component and align it with others using flex

--- src/App.js
+++ src/App.js
@@ -5,15 +5,19 @@ import CategoryContainer from './components/CategoryContainer';
 import HorizontalContainer from './components/HorizontalContainer';
 import CarouselContainer from './components/CarouselContainer';
 import AccordionContainer from './components/AccordionContainer';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <div className='w-full flex flex-col justify-center items-center gap-8'>
-      <CarouselContainer/>
-      <CategoryContainer/>
-      <HorizontalContainer isTitle={true}/>
-      <AccordionContainer/>
-      <GridContainer/>
+    <div className='w-full flex flex-col justify-center items-center'>
+      <Navbar/>
+      <div className='w-full flex flex-col justify-center items-center gap-8'>
+        <CarouselContainer/>
+        <CategoryContainer/>
+        <HorizontalContainer isTitle={true}/>
+        <AccordionContainer/>
+        <GridContainer/>
+      </div>
     </div>
   );
 }
