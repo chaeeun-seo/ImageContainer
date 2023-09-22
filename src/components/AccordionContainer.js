9b1f71fe8072b59eb50e14785956c1d4f7cc9e1f
chaeeun-seo <seochaeeun961126@gmail.com>
2023-09-22 11:55:03 +0900

    put title and ItemContainers in the flex with gap-3 in AccordionContainer

--- src/components/AccordionContainer.js
+++ src/components/AccordionContainer.js
@@ -7,11 +7,11 @@ export default function AccordionContainer() {
     const accordionBoxes = accordionCreators.map((creator) => (<AccordionBox key={creator.id} id={creator.id} rank={creator.rank} name={creator.name} tags={creator.tags} profile_image={creator.profile_image} items={creator.items}/>))
 
     return (
-        <>
+        <div className="w-full flex flex-col justify-center items-center gap-3">
             <h2 className='w-full max-w-6xl text-left text-lg font-bold'>{accordionData.title}</h2>
             <div id="accordion" data-accordion="collapse" data-active-classes="bg-white dark:bg-gray-900 text-gray-900 dark:text-white" data-inactive-classes="text-gray-500 dark:text-gray-400" className='w-full max-w-6xl'>
                 {accordionBoxes}
             </div>
-        </>
+        </div>
     )
 }
