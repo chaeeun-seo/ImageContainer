ae24e45dd8becc9d78140d73956fff6bd525543e
chaeeun-seo <seochaeeun961126@gmail.com>
2023-10-03 03:04:03 +0900

    add id prop to ItemContainer and link it to detail page using dynamic parameter id

--- src/components/ItemContainer.js
+++ src/components/ItemContainer.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-export default function ItemContainer({order, name, image_url, price, checkGrid}) {
+export default function ItemContainer({id, order, name, image_url, price, checkGrid}) {
     // TODO : change bg color of img
     // props.checkGrid == true : w-44, h-56
     // props.checkGrid == false : w-28, h-36
@@ -13,7 +13,7 @@ export default function ItemContainer({order, name, image_url, price, checkGrid}
         //     <div className={`grow w-full ${checkGrid ? 'h-56' : 'h-36'}`}>
         //         <img style={{backgroundColor: `${bgColor}`}} className={`grow w-full h-full object-cover bg-[#C0ACCA] rounded-lg`} alt="이미지" src={image_url}></img>
         //     </div>
-        <Link to={"/detail"} className={`${checkGrid ? 'w-44' : 'w-28'} grow`}>
+        <Link to={`/detail/${id}`} className={`${checkGrid ? 'w-44' : 'w-28'} grow`}>
             <img style={{backgroundColor: `${bgColor}`}} className={`w-full ${checkGrid ? 'h-56' : 'h-36'} object-cover bg-[#C0ACCA] rounded-lg`} alt="이미지" src={image_url}></img>
             <p className='font-bold text-sm'>{price}</p>
             <p className='truncate text-xs text-slate-500'>{name}</p>
